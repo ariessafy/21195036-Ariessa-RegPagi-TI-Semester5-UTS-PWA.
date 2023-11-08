76e32710d8b28c28d9341050df314fb0b2520dff
ariessafy <150217486+ariessafy@users.noreply.github.com>
2023-11-08 12:35:31 +0700

    Add files via upload

---
+++ simple_cv-main/js/notifikasi.js
@@ -0,0 +1,24 @@
+// Minta izin notifikasi
+if ('Notification' in window) {
+    Notification.requestPermission()
+      .then(function (permission) {
+        if (permission === 'granted') {
+            alert('Notifikasi Di izinkan');
+        } else if (permission === 'denied') {
+            // Izin diblokir
+            alert('Notifikasi diblokir');
+        }
+      });
+  }
+  
+  // Fungsi untuk menampilkan notifikasi
+  function showNotification(title, message) {
+    if ('Notification' in window && Notification.permission === 'granted') {
+      navigator.serviceWorker.ready.then(function (registration) {
+        registration.showNotification(title, {
+          body: message,
+          icon: 'icon.png'
+        });
+      });
+    }
+  }
